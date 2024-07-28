7451a5eebd69c87b1d18180b0f584402a537af41
nirajchand <149762449+nirajchand@users.noreply.github.com>
2024-07-28 16:29:41 +0545

    Database commit

--- src/voting/system/Database/Database.java
+++ src/voting/system/Database/Database.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package voting.system.Database;
+import java.sql.*;
 
-/**
- *
- * @author User
- */
-public class Database {
-    
+public interface Database {
+    Connection openConnection();
+    void closeConnection(Connection conn);
+    ResultSet runQuery(Connection conn, String query);
+    int executeUpdate(Connection conn,String query);
 }
