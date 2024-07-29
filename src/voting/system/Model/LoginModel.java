3a72ced61548dd9270ea2e628ac8dbef4b7af9da
KilDReaper <149761268+KilDReaper@users.noreply.github.com>
2024-07-29 16:46:41 +0545

    Login model

--- src/voting/system/Model/LoginModel.java
+++ src/voting/system/Model/LoginModel.java
@@ -9,5 +9,27 @@ package voting.system.Model;
  * @author User
  */
 public class LoginModel {
-    
+    private String Voter_Id;
+    private String Password; 
+
+    public LoginModel(String Voter_id, String Password){
+        this.Voter_Id = Voter_id;
+        this.Password = Password;
+    }
+
+    public String getVoter_Id() {
+        return Voter_Id;
+    }
+
+    public void setVoter_Id(String Voter_Id) {
+        this.Voter_Id = Voter_Id;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
 }
