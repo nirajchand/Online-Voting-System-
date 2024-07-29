9794eb9fb015571ae0ded23638ba68cf83aa1b1d
KilDReaper <149761268+KilDReaper@users.noreply.github.com>
2024-07-29 16:51:05 +0545

    DAO changes

--- src/voting/system/DAO/UserdDAO.java
+++ src/voting/system/DAO/UserdDAO.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package voting.system.DAO;
 
-/**
- *
- * @author User
- */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import voting.system.Model.SigninModel;
+import voting.system.Model.LoginModel;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import voting.system.Database.mysqlConnection;
+
 public class UserdDAO {
-    
+    private static final Logger LOGGER = Logger.getLogger(UserdDAO.class.getName());
+    mysqlConnection mysql = new mysqlConnection();
+    Connection conn = mysql.openConnection();
+    public boolean insertSignin(SigninModel signinModel) throws SQLException {
+        String query = "INSERT INTO users (Voter_id, fullname, Address, Contact, email, Password) VALUES (?, ?, ?, ?, ?, ?)";
+        try (PreparedStatement pstmt = conn.prepareStatement(query)) {
+            pstmt.setString(1, signinModel.getVoter_id());
+            pstmt.setString(2, signinModel.getFull_name());
+            pstmt.setString(3, signinModel.getAddress());
+            pstmt.setString(4, signinModel.getContact());
+            pstmt.setString(5, signinModel.getEmail());
+            pstmt.setString(6, signinModel.getPassword());
+            pstmt.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, "SQLException occurred while inserting signin data: ", e);
+            return false;
+        }
+    }
 }
