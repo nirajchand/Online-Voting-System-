2c0e61ab5ba42755be5ebcadd39837618bf726e2
aryan123jung <149761592+aryan123jung@users.noreply.github.com>
2024-08-01 19:59:36 +0545

    update signin controller

--- src/voting/system/Controller/SigninController.java
+++ src/voting/system/Controller/SigninController.java
@@ -1,13 +1,55 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package voting.system.Controller;
 
-/**
- *
- * @author User
- */
+import voting.system.Model.SigninModel;
+import voting.system.DAO.UserdDAO;
+import voting.system.View.Signin;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+
 public class SigninController {
-    
+    private final Signin signin;
+    private final UserdDAO userdao;
+
+    public SigninController(Signin signinin, UserdDAO userdao) {
+        this.signin = signinin;
+        this.userdao = userdao;
+
+
+        this.signin.addRegisterButtonListener(new RegisterButtonListener());
+    }
+
+    class RegisterButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String fullName = signin.getFullname_entry().getText();
+            String address = signin.getAddress_entry().getText();
+            String voterId = signin.getVoter_ID_entry().getText();
+            String email = signin.getEmail_entry().getText();
+            String contact = signin.getPhone_no_entry().getText();
+            String password = signin.getPassword_entry().getText();
+            String confirmPassword = signin.getConform_pass_entry().getText();
+
+            if (!password.equals(confirmPassword)) {
+                signin.setMessage("Passwords do not match!");
+                return;
+            }
+
+            SigninModel signinmodel = new SigninModel( fullName, address, voterId, email, contact, password, confirmPassword);
+            boolean isRegistered = true;
+            try{
+                isRegistered = userdao.insertSignin(signinmodel);
+            }catch(SQLException ex){
+                System.out.println(ex);
+            }
+
+            if (isRegistered){
+                signin.setMessage("Registration successful!");
+            } else {
+                signin.setMessage("Registration failed. Please try again.");
+            }
+        }
+    }
+
 }
