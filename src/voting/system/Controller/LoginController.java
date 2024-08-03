e894558823618047da7ce91e6d0acf9b3af52fb0
Rajukmahato <151640078+Rajukmahato@users.noreply.github.com>
2024-08-03 21:27:00 +0545

    Update Controller

--- src/voting/system/Controller/LoginController.java
+++ src/voting/system/Controller/LoginController.java
@@ -1,13 +1,58 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package voting.system.Controller;
 
-/**
- *
- * @author User
- */
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import voting.system.View.LogIn;
+import voting.system.DAO.UserdDAO;
+import voting.system.Model.LoginModel;
+import voting.system.View.Dashboard;
+import voting.system.View.CandidatePage;
+
 public class LoginController {
-    
+    private final UserdDAO userdao;
+    private final LogIn login;
+
+    public LoginController(UserdDAO userdao, LogIn login) {
+        this.userdao = userdao;
+        this.login = login;
+
+        this.login.LoginButton(new loginButtonListener());
+    }
+
+    class loginButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String voter_ID = login.getVoterID_entry().getText().trim();
+            String password = login.getPassword_entry().getText().trim();
+
+
+            String adminVoterID = "10";
+            String adminPassword = "admin10";
+
+            if (adminVoterID.equals(voter_ID) && adminPassword.equals(password)) {
+
+                CandidatePage adminDashboard = new CandidatePage();
+                adminDashboard.setVisible(true);
+            } else {
+                LoginModel loginmodel = new LoginModel(voter_ID, password);
+                boolean isRegularUser = false;
+                try {
+                    isRegularUser = userdao.getdata(loginmodel);
+                } catch (SQLException ex) {
+                    Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+                if (isRegularUser) {
+                    Dashboard dashboard = new Dashboard();
+                    dashboard.setVisible(true);
+
+                } else {
+                    login.setMessage("Login failed.");
+                }
+            }
+        }
+    }
 }
